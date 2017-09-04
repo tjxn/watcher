c1037596be0e3daca29898fe8dfdec873f86f33a
Trevor Jackson <trevor@trevorjackson.ca>
2017-09-03 19:52:30 -0700

    Log Config on startup. Load recheck interval from .json file

--- ping/src/ping.go
+++ ping/src/ping.go
@@ -9,13 +9,17 @@ import (
 	 "time"
 	 "io/ioutil"
 	 "github.com/golang/glog"
+	 "strconv"
 )
 
 const FIVE_SECONDS int64 = 5000000000
 const SIXTY_SECONDS int64 = 60000000000
+const DEFAULT_ENDPOINT string = "www.trevorjackson.ca:80"
+
 
 type Config struct {
-	Endpoint string `json:"endpoint"`
+	Endpoints []string `json:"endpoints"`
+	CheckInterval int64 `json:"checkInterval"`
 }
 
 func init(){
@@ -35,39 +39,46 @@ func main() {
 	glog.Info("Starting Ping\n")
 
 	config := getConfig()
-	doEvery(time.Duration(FIVE_SECONDS), checkAllEndpoints, config)
+	doEvery(time.Duration(FIVE_SECONDS), checkAllEndpoints, config.Endpoints)
 
 	glog.Info("Exiting Ping")
 	glog.Flush()
 	return
 }
 
-func getConfig() []Config{
+func getConfig() Config{
 	var config []Config
 	raw, err := ioutil.ReadFile("./config.json")
 	if (err != nil){
-		glog.Warning("Unable to Load the Configuration File, Using Default Endpoint: www.trevorjackson.ca:80")
-		var defaultEndpoint Config
-		defaultEndpoint.Endpoint = "www.trevorjackson.ca:80"
-		config = make([]Config, 1)
-		config[0] = defaultEndpoint
-		return config
+		glog.Warning("Unable to Load the Configuration File, Using Default Configuration")
+		return createDefaultConfig()
 	}
 	
 	json.Unmarshal(raw, &config)
-
-	return config
+	glog.Info("Endpoints: " + fmt.Sprintf("%#v\n", config[0].Endpoints))
+	glog.Info("CheckInterval: " +  strconv.FormatInt(config[0].CheckInterval, 10))
+	return config[0]
 }
 
-func doEvery(d time.Duration, f func([]Config), endpoints []Config) {
+func createDefaultConfig() Config {
+	defaultConfig := new(Config)
+	var defaultEndpoint = []string{DEFAULT_ENDPOINT}
+	defaultConfig.Endpoints = defaultEndpoint
+	defaultConfig.CheckInterval = FIVE_SECONDS
+	glog.Info("Endpoints: " + fmt.Sprintf("%#v\n", defaultConfig.Endpoints))
+	glog.Info("CheckInterval: " +  strconv.FormatInt(defaultConfig.CheckInterval, 10))
+	return *defaultConfig
+}
+
+func doEvery(d time.Duration, f func([]string), endpoints []string) {
 	for range time.Tick(d) {
 		f(endpoints)
 	}
 }
 
-func checkAllEndpoints(endpoints []Config){
+func checkAllEndpoints(endpoints []string){
 	for _, elment := range endpoints {
-		checkEndpoint(elment.Endpoint)
+		checkEndpoint(elment)
 	}
 }
 
